60b510fa851a977fe01909e2ce99f1868b910dc7
Shucharjer <Shucharjer@outlook.com>
2025-12-22 00:21:11 +0800

    [electron] add hello world for example

--- electron/example/src/main.cpp
+++ electron/example/src/main.cpp
@@ -1,3 +1,4 @@
+#include <imgui.h>
 #include <proton/proton.hpp>
 #include <proton/run.hpp>
 #include <proton/system.hpp>
@@ -9,15 +10,19 @@ using namespace proton;
 using namespace electron;
 using enum stage;
 
-void print() {}
+void renderHello() {
+    ImGui::Begin("window");
+    ImGui::Text("hello world"); // NOLINT
+    ImGui::End();
+}
 
 int main() {
-    constexpr auto world = world_desc | add_system<update, &print>;
+    constexpr auto world = world_desc | add_system<render, &renderHello>;
     const auto config    = wnd_config{ .name   = "example",
                                        .width  = 1280,
                                        .height = 960,
                                        .flags  = window_flags::none };
-    App::create() | run_worlds<world>(config);
+    App::Create() | run_worlds<world>(config);
 
     return 0;
 }
